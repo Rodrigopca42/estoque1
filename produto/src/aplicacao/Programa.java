8fcdc8fedec8276428a4914d2a1a37a622e288c8
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-07-28 18:04:26 -0300

    readme
    readme

--- produto/src/aplicacao/Programa.java
+++ produto/src/aplicacao/Programa.java
@@ -7,26 +7,25 @@ import entidade.Produto;
 public class Programa {
     public static void main(String[] args) {
         
-        Scanner tec = new Scanner(System.in);
-
-        Produto produto = new Produto();
+        Scanner tec = new Scanner(System.in);  
         
         System.out.print("Entre com o Produto:");
-        produto.nome = tec.nextLine();
-
+        String nome = tec.nextLine();
+        
         System.out.print("Preço: ");
-        produto.preco = tec.nextDouble();
-
+        double preco = tec.nextDouble();
+        
         System.out.print("Quantidade em estoque: ");
-        produto.quantidade = tec.nextInt();
-
+        int quantidade = tec.nextInt();
+        
+        Produto produto = new Produto(nome, preco, quantidade);
         
         System.out.println(" ");
         System.out.println("Produtos em estoque: "+ produto);
         System.out.println(" ");
 
         System.out.print("Entre com a quantidade que será adicionado ao estoque: ");
-        int quantidade = tec.nextInt();
+        quantidade = tec.nextInt();
         produto.adicionarProdutos(quantidade);
 
         System.out.println(" ");
